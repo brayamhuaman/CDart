654adaa26664ee2c70350af57d67ad2746084f46
Brayam <bhuamanpc@gmail.com>
2024-06-12 18:23:05 -0500

    condicionales

--- operadoresrelacionales.dart
+++ operadoresrelacionales.dart
@@ -2,7 +2,7 @@ void main(){
   var precio = 500;
   if(precio==500){
     print("Precio promedio");
-  }else if(precio > 500) {
+  }else if(precio > 500 || precio==0 ) {
     print("Precio mayor del promedio");
   }else{
     print("precio es menor al promedio");
